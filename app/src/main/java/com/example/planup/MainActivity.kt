1eeb19ac84ae36b1bebf17d4b874c71a8f95c932
seonjun <parkseonjun@seonjunui-MacBookAir.local>
2025-07-16 20:02:56 +0900

    [Mod] 바텀 네비게이션 수정

--- app/src/main/java/com/example/planup/MainActivity.kt
+++ app/src/main/java/com/example/planup/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.updatePadding
 import com.example.planup.mypage.MypageFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationBarView
 
 class MainActivity : AppCompatActivity() {
@@ -18,62 +19,51 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        initBottomNavigation()
+    }
 
-        binding.bottomNavigationView.labelVisibilityMode =
-            NavigationBarView.LABEL_VISIBILITY_LABELED
+    private fun initBottomNavigation() {
 
-        binding.bottomNavigationView.isItemHorizontalTranslationEnabled = false
-
-        // 앱 실행 시 기본 Fragment 설정
+        binding.bottomNavigationView.selectedItemId = R.id.fragment_home
         supportFragmentManager.beginTransaction()
             .replace(R.id.main_container, HomeFragment())
             .commitAllowingStateLoss()
 
-        // BottomNavigationView 리스너 설정
         binding.bottomNavigationView.setOnItemSelectedListener { item ->
             when (item.itemId) {
+
+                R.id.fragment_goal -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_container,GoalFragment())
+                        .commitAllowingStateLoss()
+                    return@setOnItemSelectedListener true
+                }
+                R.id.fragment_record -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_container,RecordFragment())
+                        .commitAllowingStateLoss()
+                    return@setOnItemSelectedListener true
+                }
                 R.id.fragment_home -> {
                     supportFragmentManager.beginTransaction()
                         .replace(R.id.main_container, HomeFragment())
                         .commitAllowingStateLoss()
-                    true
+                    return@setOnItemSelectedListener true
                 }
-
-                R.id.fragment_goal -> {
-                    supportFragmentManager.beginTransaction()
-                        .replace(R.id.main_container, GoalFragment())
-                        .commitAllowingStateLoss()
-                    true
-                }
-
-                R.id.fragment_record -> {
-                    supportFragmentManager.beginTransaction()
-                        .replace(R.id.main_container, RecordFragment())
-                        .commitAllowingStateLoss()
-                    true
-                }
-
                 R.id.fragment_friend -> {
                     supportFragmentManager.beginTransaction()
-                        .replace(R.id.main_container, FriendFragment())
+                        .replace(R.id.main_container,FriendFragment())
                         .commitAllowingStateLoss()
-                    true
+                    return@setOnItemSelectedListener true
                 }
-
                 R.id.fragment_mypage -> {
                     supportFragmentManager.beginTransaction()
-                        .replace(R.id.main_container, MypageFragment())
+                        .replace(R.id.main_container,MypageFragment())
                         .commitAllowingStateLoss()
-                    true
+                    return@setOnItemSelectedListener true
                 }
-
-                else -> false
             }
-        }
-        ViewCompat.setOnApplyWindowInsetsListener(binding.bottomNavigationView) { view, insets ->
-            val systemBarsInsets = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            view.updatePadding(bottom = systemBarsInsets.bottom + 10)  // 추가 padding 10dp
-            insets
+            false
         }
     }
 }
