80c946895ed2f4a8a808dbda292cc49fb5e099a5
seonjun <parkseonjun@seonjunui-MacBookAir.local>
2025-07-22 16:44:14 +0900

    ddd

--- app/src/main/java/com/example/planup/SettingAlertArrayAdapter.kt
+++ app/src/main/java/com/example/planup/SettingAlertArrayAdapter.kt
@@ -3,7 +3,7 @@ package com.example.planup
 import android.content.Context
 import android.widget.ArrayAdapter
 
-class SettingAlertArrayAdapter(
-    context: Context
-) : ArrayAdapter<T>(context, R.layout.item_spinner, R.id.tv_title, arrayListOf()) {
-}
+//class SettingAlertArrayAdapter(
+//    context: Context
+//) : ArrayAdapter<T>(context, R.layout.item_spinner, R.id.tv_title, arrayListOf()) {
+//}
