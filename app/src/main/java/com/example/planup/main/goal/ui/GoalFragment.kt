938129957a90a892ed250810fb856d3427f58132
james4510 <james451055@gmail.com>
2025-07-27 15:30:49 +0900

    feat/목표 페이지 추가

--- app/src/main/java/com/example/planup/main/goal/ui/GoalFragment.kt
+++ app/src/main/java/com/example/planup/main/goal/ui/GoalFragment.kt
@@ -23,31 +23,35 @@ class GoalFragment : Fragment() {
         return binding.root
     }
 
+//    private fun clickListener(){
+////        // 알림
+////        binding.challengeMainAlertIv.setOnClickListener{}
+////        // 주석
+////        binding.challengeMainTodoIv.setOnClickListener{}
+////        // 함께 도전 중인 친구들
+////        binding.challengeMainFriendTitleIv.setOnClickListener{}
+//    }
+//
+//    /*챌린지 참여 요청 팝업*/
+//    private fun makePopup(){
+//        val dialog = Dialog(context as MainActivity)
+//        dialog.setContentView(R.layout.popup_challenge)
+//        dialog.window?.apply {
+//            setLayout(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT)
+//            setGravity(Gravity.BOTTOM)
+//        }
+//
+//        dialog.findViewById<View>(R.id.popup_challenge_check_btn_iv).setOnClickListener{
+//            (context as MainActivity).supportFragmentManager.beginTransaction()
+//                .replace(R.id.main_container, GoalFragment())
+//                .commitAllowingStateLoss()
+//        }
+//        dialog.setCanceledOnTouchOutside(false)
+//        dialog.show()
+//    }
+
     private fun clickListener(){
-//        // 알림
-//        binding.challengeMainAlertIv.setOnClickListener{}
-//        // 주석
-//        binding.challengeMainTodoIv.setOnClickListener{}
-//        // 함께 도전 중인 친구들
-//        binding.challengeMainFriendTitleIv.setOnClickListener{}
-    }
 
-    /*챌린지 참여 요청 팝업*/
-    private fun makePopup(){
-        val dialog = Dialog(context as MainActivity)
-        dialog.setContentView(R.layout.popup_challenge)
-        dialog.window?.apply {
-            setLayout(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT)
-            setGravity(Gravity.BOTTOM)
-        }
-
-        dialog.findViewById<View>(R.id.popup_challenge_check_btn_iv).setOnClickListener{
-            (context as MainActivity).supportFragmentManager.beginTransaction()
-                .replace(R.id.main_container, GoalFragment())
-                .commitAllowingStateLoss()
-        }
-        dialog.setCanceledOnTouchOutside(false)
-        dialog.show()
     }
 
 }
