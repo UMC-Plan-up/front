f89b00fd4b9f7055be253032e6d0d4996a7ae36f
seonjun <parkseonjun@seonjunui-MacBookAir.local>
2025-08-01 02:07:48 +0900

    [style] 드롭다운 구현
    리사이클러뷰를 이용한 드롭다운 타이머, 알림설정 프레그먼트에 적용

--- app/src/main/java/com/example/planup/goal/ui/ChallengeSetTimerFragment.kt
+++ app/src/main/java/com/example/planup/goal/ui/ChallengeSetTimerFragment.kt
@@ -19,6 +19,7 @@ import java.sql.Time
 
 class ChallengeSetTimerFragment:Fragment() {
     lateinit var binding: FragmentChallengeSetTimerBinding
+    private var totalTime = 0
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -64,7 +65,6 @@ class ChallengeSetTimerFragment:Fragment() {
         val hours = resources.getStringArray(R.array.spinner_hour).toCollection(ArrayList<String>()) //시간
         val minutes = resources.getStringArray(R.array.spinner_minute_second).toCollection(ArrayList<String>()) //분
         val seconds = resources.getStringArray(R.array.spinner_minute_second).toCollection(ArrayList<String>()) //초
-        Log.d("afadsfadsfadshgkljdfshgiru", R.array.spinner_minute_second.toString())
 
         //각 드롭다운에 대한 어댑터 생성
         val timerAdapter = ArrayList<TimerRVAdapter>()
@@ -79,26 +79,49 @@ class ChallengeSetTimerFragment:Fragment() {
         timerAdapter[0].setDropdownListener(object : TimerRVAdapter.DropdownListener{
             //시간 선택
             override fun setTime(position: Int) {
-                Log.d("afdfdaff","afafdfafaffffa hour touch")
                 binding.challengeTimerHourTv.text = hours[position]
                 binding.challengeTimerHourRv.visibility = View.GONE
+                timeWatcher(3600*hours[position].toInt(),0) //전체 시간 업데이트
             }
         })
         timerAdapter[1].setDropdownListener(object : TimerRVAdapter.DropdownListener{
             //분 선택
             override fun setTime(position: Int) {
-                Log.d("afdfdaff","afafdfafaffffa minute click")
                 binding.challengeTimerMinuteTv.text = minutes[position]
                 binding.challengeTimerMinuteRv.visibility = View.GONE
+                timeWatcher(minutes[position].toInt(),1) //전체 시간 업데이트
             }
         })
         timerAdapter[2].setDropdownListener(object : TimerRVAdapter.DropdownListener{
             //초 선택
             override fun setTime(position: Int) {
-                Log.d("afdfdaff","afafdfafaffffa second click")
                 binding.challengeTimerSecondTv.text = seconds[position]
                 binding.challengeTimerSecondRv.visibility = View.GONE
+                timeWatcher(seconds[position].toInt(),2) //전체 시간 업데이트
             }
         })
+    }
+    //타이머로 설정한 시간 업데이트
+    //마지막 조건문으로 전체 시간이 30초 이상인지 확인
+    private fun timeWatcher(selected:Int,position:Int){
+        val hour = (totalTime/3600)*3600
+        val minute = (totalTime-(totalTime/3600)*3600)/60
+        val second= totalTime - ((totalTime-(totalTime/3600)*3600)/60)
+
+        if (position==0){
+            totalTime -= hour
+            totalTime += 3600*selected
+        } else if (position == 1){
+            totalTime -= minute
+            totalTime += 60*selected
+        } else{
+            totalTime -= second
+            totalTime += selected
+        }
+        if (totalTime < 30){
+            binding.errorTv.visibility = View.VISIBLE
+        }else{
+            binding.errorTv.visibility = View.GONE
+        }
     }
 }
