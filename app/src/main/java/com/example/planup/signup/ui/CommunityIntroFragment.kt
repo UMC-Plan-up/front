a7719cd05805a072bfc53e9e212e1b7d3fcd5db9
seonjun <parkseonjun@seonjunui-MacBookAir.local>
2025-08-01 22:43:31 +0900

    [feat] 타이머 드롭다운
    - 타이머 드롭다운에 사용하던 스피너 뷰를 리사이클러 뷰로 교체함
    - 나머지 타이머 설정 및 드롭다운 뷰도 교체할 예정

--- app/src/main/java/com/example/planup/signup/ui/CommunityIntroFragment.kt
+++ app/src/main/java/com/example/planup/signup/ui/CommunityIntroFragment.kt
@@ -144,6 +144,7 @@ class CommunityIntroFragment : Fragment(R.layout.fragment_community_intro) {
 
         /* 목표 설정 시작하기 버튼 → GoalCategoryFragment로 이동 */
         nextButton.setOnClickListener {
+
             val context = requireContext()
             val intent = Intent(context, GoalActivity::class.java).apply {
                 putExtra("goalOwnerName", nickname) // 닉네임 전달
