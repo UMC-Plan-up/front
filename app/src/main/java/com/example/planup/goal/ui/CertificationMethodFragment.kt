72f258e0c67a94189612ef3f11c9361bd88e77e2
seonjun <parkseonjun@seonjunui-MacBookAir.local>
2025-08-02 15:25:44 +0900

    수정
     Conflicts:
    	app/src/main/java/com/example/planup/goal/GoalActivity.kt
    	app/src/main/java/com/example/planup/goal/ui/CertificationMethodFragment.kt
    	app/src/main/java/com/example/planup/goal/ui/CommonGoalFragment.kt
    	app/src/main/java/com/example/planup/goal/ui/GoalDetailFragment.kt
    	app/src/main/java/com/example/planup/goal/ui/GoalInputFragment.kt
    	app/src/main/java/com/example/planup/goal/ui/TimerSettingFragment.kt
    	app/src/main/java/com/example/planup/signup/ui/CommunityIntroFragment.kt

--- app/src/main/java/com/example/planup/goal/ui/CertificationMethodFragment.kt
+++ app/src/main/java/com/example/planup/goal/ui/CertificationMethodFragment.kt
@@ -78,6 +78,7 @@ class CertificationMethodFragment : Fragment(R.layout.fragment_certification_met
                             putString("goalOwnerName", goalOwnerName) // 닉네임 다시 넘기기
                         }
                     }
+
                     (requireActivity() as GoalActivity).navigateToFragment(detailFragment)
                 }
                 else -> {
