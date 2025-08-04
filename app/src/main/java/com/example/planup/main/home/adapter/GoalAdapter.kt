a16f18d03eef82f3248d922e8d85e5145155a553
james4510 <james451055@gmail.com>
2025-08-04 14:45:54 +0900

    just commit

--- app/src/main/java/com/example/planup/main/home/adapter/GoalAdapter.kt
+++ app/src/main/java/com/example/planup/main/home/adapter/GoalAdapter.kt
@@ -17,7 +17,10 @@ import com.github.mikephil.charting.data.PieEntry
 
 class GoalAdapter(
     private val items: List<GoalItem>,
-    private val onItemClick: (GoalItem) -> Unit) :
+    private val onItemClick: (GoalItem) -> Unit,
+    private val onDeactivateConfirmed: () -> Unit,
+    private val onActivateConfirmed: () -> Unit,
+    private val onDeleteConfirmed: () -> Unit) :
     RecyclerView.Adapter<GoalAdapter.GoalViewHolder>() {
 
     private var isEditMode: Boolean = false
@@ -45,7 +48,7 @@ class GoalAdapter(
         setupDonutChart(holder.pieChart, item.percent)
 
         holder.buttonLayout.visibility = if (isEditMode) View.VISIBLE else View.GONE
-        holder.editIcon.visibility = if (isEditMode) View.VISIBLE else View.GONE
+        // holder.editIcon.visibility = if (isEditMode) View.VISIBLE else View.GONE
 
         holder.toggleBtn.text = if (item.isActive) "비활성화하기" else "활성화하기"
 
@@ -136,6 +139,7 @@ class GoalAdapter(
         yesBtn.setOnClickListener {
             item.isActive = !item.isActive // 상태 전환
             notifyItemChanged(position)
+            onDeactivateConfirmed()
             dialog.dismiss()
         }
 
@@ -158,6 +162,7 @@ class GoalAdapter(
         yesBtn.setOnClickListener {
             item.isActive = !item.isActive // 상태 전환
             notifyItemChanged(position)
+            onActivateConfirmed()
             dialog.dismiss()
         }
 
@@ -178,10 +183,13 @@ class GoalAdapter(
         }
 
         yesBtn.setOnClickListener {
+            onDeleteConfirmed()
             dialog.dismiss()
         }
 
         dialog.window?.setBackgroundDrawableResource(android.R.color.transparent)
         dialog.show()
     }
+
+
 }
