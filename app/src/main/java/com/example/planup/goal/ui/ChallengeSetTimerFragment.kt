4663b2faf824c9134eca1a1929779c2f4cb5cc08
seonjun <parkseonjun@seonjunui-MacBookAir.local>
2025-08-07 17:43:19 +0900

    [UI] 타이머 드롭다운 최종 수정
    피그마에 나온 높이대로 반영함

--- app/src/main/java/com/example/planup/goal/ui/ChallengeSetTimerFragment.kt
+++ app/src/main/java/com/example/planup/goal/ui/ChallengeSetTimerFragment.kt
@@ -1,5 +1,6 @@
 package com.example.planup.goal.ui
 
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -7,12 +8,14 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.PopupWindow
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import com.example.planup.R
 import com.example.planup.databinding.FragmentChallengeSetTimerBinding
 import com.example.planup.goal.GoalActivity
 import com.example.planup.goal.adapter.TimerRVAdapter
+import androidx.core.graphics.drawable.toDrawable
 
 class ChallengeSetTimerFragment:Fragment() {
     lateinit var binding: FragmentChallengeSetTimerBinding
@@ -68,48 +71,7 @@ class ChallengeSetTimerFragment:Fragment() {
         }
     }
 
-    //타이머 설정 관리
-//    private fun setTimer(){
-//        //시간, 분, 초 를 저장하는 어레이 리스트 만들기
-//        val hours = resources.getStringArray(R.array.spinner_hour).toCollection(ArrayList<String>()) //시간
-//        val minutes = resources.getStringArray(R.array.spinner_minute_second).toCollection(ArrayList<String>()) //분
-//        val seconds = resources.getStringArray(R.array.spinner_minute_second).toCollection(ArrayList<String>()) //초
-//
-//        //각 드롭다운에 대한 어댑터 생성
-//        val timerAdapter = ArrayList<TimerRVAdapter>()
-//        timerAdapter.add(0, TimerRVAdapter(hours))
-//        timerAdapter.add(1, TimerRVAdapter(minutes))
-//        timerAdapter.add(2, TimerRVAdapter(seconds))
-//
-//        binding.challengeTimerHourRv.adapter = timerAdapter[0]
-//        binding.challengeTimerMinuteRv.adapter = timerAdapter[1]
-//        binding.challengeTimerSecondRv.adapter = timerAdapter[2]
-//
-//        timerAdapter[0].setDropdownListener(object : TimerRVAdapter.DropdownListener{
-//            //시간 선택
-//            override fun setTime(position: Int) {
-//                binding.challengeTimerHourTv.text = hours[position]
-//                binding.challengeTimerHourRv.visibility = View.GONE
-//                timeWatcher(hours[position].toInt(),0) //전체 시간 업데이트
-//            }
-//        })
-//        timerAdapter[1].setDropdownListener(object : TimerRVAdapter.DropdownListener{
-//            //분 선택
-//            override fun setTime(position: Int) {
-//                binding.challengeTimerMinuteTv.text = minutes[position]
-//                binding.challengeTimerMinuteRv.visibility = View.GONE
-//                timeWatcher(minutes[position].toInt(),1) //전체 시간 업데이트
-//            }
-//        })
-//        timerAdapter[2].setDropdownListener(object : TimerRVAdapter.DropdownListener{
-//            //초 선택
-//            override fun setTime(position: Int) {
-//                binding.challengeTimerSecondTv.text = seconds[position]
-//                binding.challengeTimerSecondRv.visibility = View.GONE
-//                timeWatcher(seconds[position].toInt(),2) //전체 시간 업데이트
-//            }
-//        })
-//    }
+
 
     //타이머로 설정한 시간 업데이트
     //마지막 조건문으로 전체 시간이 30초 이상인지 확인
@@ -129,7 +91,6 @@ class ChallengeSetTimerFragment:Fragment() {
             totalTime += item
         }
         if (totalTime < 30){
-            Log.d("qhrkqelw;i","${totalTime} ${item}")
             binding.errorTv.visibility = View.VISIBLE
         }else{
             binding.errorTv.visibility = View.GONE
@@ -142,12 +103,17 @@ class ChallengeSetTimerFragment:Fragment() {
         val inflater = LayoutInflater.from(context)
         val popupView = inflater.inflate(R.layout.item_recycler_dropdown,null)
         val popupWindow = PopupWindow(
-            popupView, ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT
+            popupView,
+            ViewGroup.LayoutParams.WRAP_CONTENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT,
+            true
         )
         val dropdownAdapter = TimerRVAdapter(items)
         popupWindow.showAsDropDown(view) //선택된 뷰 하단에 드롭다운 표시
         popupWindow.isOutsideTouchable = true //바깥 터치 허용
+        popupWindow.setBackgroundDrawable(resources.getColor(R.color.transparent).toDrawable())//투명 배경 설정
 
+        //드롭다운 터치 이벤트 관리하는 어댑터
         popupView.findViewById<RecyclerView>(R.id.dropdown_recycler_rv).adapter = dropdownAdapter
         dropdownAdapter.setDropdownListener(object : TimerRVAdapter.DropdownListener{
             override fun setTime(position: Int) {
