b8d09c2d2d5771ee9cf2792154d5b2bc4fb5d85e
james4510 <james451055@gmail.com>
2025-08-10 23:50:29 +0900

    FEAT:목표리스트 반환 API 구현

---
+++ app/src/main/java/com/example/planup/main/record/adapter/DetailWeeklyReportResponse.kt
@@ -0,0 +1,43 @@
+package com.example.planup.main.record.adapter
+
+data class DetailWeeklyReportResponse(
+    val isSuccess: Boolean,
+    val code: String,
+    val message: String,
+    val result: DetailWeeklyReportResult
+)
+
+data class DetailWeeklyReportResult(
+    val id: Int,
+    val year: Int,
+    val month: Int,
+    val weekNumber: Int,
+    val startDate: String,
+    val endDate: String,
+    val nextGoalMessage: String,
+    val goalReports: List<GoalReport>,
+    val totalDailyAchievement: Int,
+    val dailyRecordList: List<DailyRecord>,
+    val badgeList: List<Badge>
+)
+
+data class GoalReport(
+    val id: Int,
+    val goalTitle: String,
+    val goalCriteria: String,
+    val achievementRate: Int
+)
+
+data class DailyRecord(
+    val id: Int,
+    val date: String,
+    val recordedTime: Int,
+    val photoVerified: String,
+    val simpleMessage: String
+)
+
+data class Badge(
+    val id: Int,
+    val badgeName: String,
+    val badgeType: String
+)
