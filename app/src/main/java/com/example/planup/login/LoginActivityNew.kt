066762618c2b445c2a4742e70817fe83b5d9a6e8
seonjun <parkseonjun@seonjunui-MacBookAir.local>
2025-08-19 18:33:14 +0900

    [feat] 친구 차단,삭제,신고 API

--- app/src/main/java/com/example/planup/login/LoginActivityNew.kt
+++ app/src/main/java/com/example/planup/login/LoginActivityNew.kt
@@ -272,7 +272,6 @@ class LoginActivityNew: AppCompatActivity(), LoginAdapter, UserInfoAdapter {
 
     //유저 정보 요청 통신 성공
     override fun successUserInfo(user: UserInfo) {
-        Log.d("okhttp",user.profileImage)
         // 유저 정보와 토큰을 함께 저장하고 메인으로 이동하는 통합 함수 호출
         saveUserInfoAndGoToMain(
             user.id,
