4cc29249e45fc490a42fe9dbe6d691b2f6447336
miyeon04 <miyeoni241@gmail.com>
2025-08-20 15:24:43 +0900

    feat/login

--- app/src/main/java/com/example/planup/signup/SignupActivity.kt
+++ app/src/main/java/com/example/planup/signup/SignupActivity.kt
@@ -1,8 +1,6 @@
 package com.example.planup.signup
 
 import android.content.Intent
-import android.content.SharedPreferences
-import android.content.SharedPreferences.Editor
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
