978c1af83912c2ff946171c7d5e65f49b756bba1
jmseb3 <jmseb3@naver.com>
2025-10-05 22:05:17 +0900

    feat: Network 모듈 적용 예시 추가

---
+++ app/src/main/java/com/example/planup/di/NetworkModule.kt
@@ -0,0 +1,43 @@
+package com.example.planup.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+class NetworkModule {
+
+    companion object {
+        private const val BASE_URL = "http://54.180.207.84:8080/"
+    }
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(): OkHttpClient {
+        return OkHttpClient
+            //TODO Add Interceptor
+            .Builder()
+            .connectTimeout(20, TimeUnit.SECONDS)
+            .readTimeout(30, TimeUnit.SECONDS)
+            .writeTimeout(30, TimeUnit.SECONDS)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+}
