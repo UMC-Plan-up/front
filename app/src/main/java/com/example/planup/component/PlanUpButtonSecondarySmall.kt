dceea8b4c3eb956617f53c2471bd2736484386bb
jmseb3 <jmseb3@naver.com>
2025-10-20 23:43:40 +0900

    feat: Small Button Component 추가
    - PlanUpButtonSmall Composable을 추가했습니다.
    - PlanUpButtonSecondarySmall Composable을 추가했습니다.

---
+++ app/src/main/java/com/example/planup/component/PlanUpButtonSecondarySmall.kt
@@ -0,0 +1,61 @@
+package com.example.planup.component
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.planup.theme.BLACK100
+import com.example.planup.theme.BLACK200
+import com.example.planup.theme.BLACK300
+import com.example.planup.theme.BLUE200
+import com.example.planup.theme.Typography
+import com.example.planup.theme.fontColor
+import com.example.planup.theme.pretendard
+
+@Composable
+fun PlanUpButtonSecondarySmall(
+    modifier: Modifier = Modifier,
+    title: String,
+    onClick: () -> Unit
+) {
+    Button(
+        onClick = onClick,
+        enabled = true,
+        modifier = Modifier
+            .height(36.dp)
+            .then(modifier),
+        colors = ButtonDefaults.buttonColors(
+            containerColor = BLACK100,
+        ),
+        shape = RoundedCornerShape(6.dp)
+    ) {
+        Text(
+            text = title,
+            color = BLACK300,
+            fontFamily = pretendard,
+        )
+    }
+}
+
+
+@Preview
+@Composable
+private fun PlanUpButtonSecondarySmallPreview() {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(10.dp)
+    ) {
+        PlanUpButtonSecondarySmall(
+            modifier = Modifier.width(320.dp),
+            title = "다음",
+            onClick = {}
+        )
+    }
+}
--- app/src/main/java/com/example/planup/theme/Color.kt
+++ app/src/main/java/com/example/planup/theme/Color.kt
@@ -4,7 +4,9 @@ import androidx.compose.ui.graphics.Color
 
 val BLUE200 = Color(0xff5C91FC)
 
+val BLACK100 = Color(0xffE4E6E8)
 val BLACK200 = Color(0xffC7CACF)
+val BLACK300 = Color(0xff4B5563)
 val BLACK400 = Color(0xff22262D)
 
 val fontColor = Color(0xffFDFEFF)
