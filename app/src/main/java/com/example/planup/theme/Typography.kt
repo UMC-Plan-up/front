d7f4920b96e60ea22d1937654a78412132872a24
jmseb3 <jmseb3@naver.com>
2025-10-20 23:02:48 +0900

    feat: PlanUp 공용 버튼 구현
    - Button에 필요한 Color, Typography 추가

---
+++ app/src/main/java/com/example/planup/theme/Typography.kt
@@ -0,0 +1,24 @@
+package com.example.planup.theme
+
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.example.planup.R
+
+val pretendard = FontFamily(
+    Font(R.font.pretendard_regular, FontWeight.Normal),
+    Font(R.font.pretendard_medium, FontWeight.Medium),
+    Font(R.font.pretendard_semibold, FontWeight.SemiBold),
+    Font(R.font.pretendard_bold, FontWeight.Bold),
+)
+
+object Typography {
+    val medium_SM
+        get() = TextStyle(
+            fontFamily = pretendard,
+            fontSize = 16.sp,
+            fontWeight = FontWeight.Medium,
+        )
+}
