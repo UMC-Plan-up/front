6dd2a69ae34cd35d2007c6d04cf89e450787dfd2
Dohyeok Kim <hyeokdokim@gmail.com>
2025-10-21 12:15:14 +0900

    chore: main conflict 해결

--- app/src/main/java/com/example/planup/login/ui/LoginViewModel.kt
+++ app/src/main/java/com/example/planup/login/ui/LoginViewModel.kt
@@ -1,7 +1,7 @@
 package com.example.planup.login.ui
 
 import androidx.lifecycle.ViewModel
-import com.example.planup.network.repository.UserRepository
+import com.example.planup.main.user.domain.UserRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
--- app/src/main/java/com/example/planup/network/repository/UserRepository.kt
+++
@@ -1,5 +0,0 @@
-package com.example.planup.network.repository
-
-interface UserRepository {
-
-}
