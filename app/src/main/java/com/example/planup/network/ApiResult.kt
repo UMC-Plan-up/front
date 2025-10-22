8539965f036c2640ffd50e7ffb18ad0bdc8768e8
jmseb3 <jmseb3@naver.com>
2025-10-22 10:00:54 +0900

    feat: ApiResult 성공/실패 처리 확장 함수 추가

--- app/src/main/java/com/example/planup/network/ApiResult.kt
+++ app/src/main/java/com/example/planup/network/ApiResult.kt
@@ -10,6 +10,36 @@ sealed class ApiResult<out T> {
     data class Exception(val error: Throwable) : ApiResult<Nothing>()
 }
 
+inline fun <T> ApiResult<T>.onSuccess(
+    onSuccessAction: (data: T) -> Unit
+): ApiResult<T> {
+    if (this is ApiResult.Success) {
+        onSuccessAction(this.data)
+    }
+    return this
+}
+
+inline fun <T> ApiResult<T>.onFailWithMessage(
+    onFailAction: (message: String) -> Unit
+): ApiResult<T> {
+    when (this) {
+        is ApiResult.Fail -> {
+            onFailAction(this.message)
+        }
+
+        is ApiResult.Error -> {
+            onFailAction(this.message)
+        }
+
+        is ApiResult.Exception -> {
+            onFailAction(this.error.message ?: "Unknown Error")
+        }
+
+        else -> return this
+    }
+    return this
+}
+
 /**
  * @param response API 콜을 통한 Response<T> 응답
  * @param onResponse 응답 성공시 T를 기반으로 ApiResult<R> 결과값 반환
