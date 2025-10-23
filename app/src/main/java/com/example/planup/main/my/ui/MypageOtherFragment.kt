cd5f2ff70e15453cb686599f486c6475f1e3b107
jmseb3 <jmseb3@naver.com>
2025-10-23 15:17:46 +0900

    feat: 로그아웃 기능 및 팝업 구현
    - '로그아웃' 메뉴 클릭 시 로그아웃 여부를 묻는 확인 팝업(Dialog)을 표시합니다.
    - 로그아웃, 회원탈퇴, 팝업 관련 string resource를 추가했습니다.

--- app/src/main/java/com/example/planup/main/my/ui/MypageOtherFragment.kt
+++ app/src/main/java/com/example/planup/main/my/ui/MypageOtherFragment.kt
@@ -12,15 +12,33 @@ import android.view.ViewTreeObserver
 import android.widget.TextView
 import android.widget.Toast
 import android.widget.Toast.LENGTH_SHORT
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.OutlinedCard
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.core.graphics.drawable.toDrawable
 import androidx.fragment.app.Fragment
 import com.example.planup.R
+import com.example.planup.component.PlanUpBorderButton
+import com.example.planup.component.PlanUpButton
 import com.example.planup.databinding.FragmentMypageOtherBinding
 import com.example.planup.login.ui.LoginActivityNew
 import com.example.planup.main.MainActivity
@@ -28,6 +46,7 @@ import com.example.planup.main.my.adapter.LogoutAdapter
 import com.example.planup.main.my.ui.common.RouteMenuItem
 import com.example.planup.main.my.ui.common.RoutePageDefault
 import com.example.planup.network.controller.UserController
+import com.example.planup.theme.Typography
 
 class MypageOtherFragment : Fragment(), LogoutAdapter {
     lateinit var binding: FragmentMypageOtherBinding
@@ -120,19 +139,85 @@ class MypageOtherFragment : Fragment(), LogoutAdapter {
 @Composable
 fun MyPageOtherView(
     onBack: () -> Unit,
-    navigateDelete :() -> Unit,
+    navigateDelete: () -> Unit,
 ) {
+    var showLogoutDialog by rememberSaveable() {
+        mutableStateOf(false)
+    }
     RoutePageDefault(
         onBack = onBack,
         categoryText = stringResource(R.string.category)
     ) {
         Spacer(Modifier.height(20.dp))
         RouteMenuItem(
-            title = "로그아웃"
-        ) { }
+            title = stringResource(R.string.mypage_other_logout),
+            action = {
+                showLogoutDialog = true
+            }
+        )
         RouteMenuItem(
-            title = "회원 탈퇴",
+            title = stringResource(R.string.mypage_other_delete),
             action = navigateDelete
         )
+    }
+    if (showLogoutDialog) {
+        LogoutDialog(
+            onDismissRequest = { showLogoutDialog = false },
+            logout = {
+
+            }
+        )
+    }
+}
+
+
+@Composable
+private fun LogoutDialog(
+    onDismissRequest: () -> Unit,
+    logout: () -> Unit = {}
+) {
+    androidx.compose.ui.window.Dialog(
+        onDismissRequest = onDismissRequest
+    ) {
+        OutlinedCard(
+            shape = RoundedCornerShape(10.dp),
+            border = BorderStroke(1.dp,androidx.compose.ui.graphics.Color(0xffdadada)),
+            colors = CardDefaults.outlinedCardColors(
+                containerColor = androidx.compose.ui.graphics.Color.White
+            )
+        ) {
+            Column(
+                modifier = Modifier
+                    .height(140.dp)
+                    .padding(12.dp),
+                verticalArrangement = Arrangement.SpaceBetween,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Box(
+                    modifier = Modifier.weight(1f)
+                ) {
+                    Text(
+                        modifier = Modifier.align(Alignment.Center),
+                        text = stringResource(R.string.popup_ask_logout),
+                        style = Typography.Medium_SM
+                    )
+                }
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.spacedBy(6.dp)
+                ) {
+                    PlanUpBorderButton(
+                        modifier = Modifier.weight(1f),
+                        title = stringResource(R.string.btn_cancel),
+                        onClick = onDismissRequest
+                    )
+                    PlanUpButton(
+                        modifier = Modifier.weight(1f),
+                        title = stringResource(R.string.btn_ok),
+                        onClick = logout
+                    )
+                }
+            }
+        }
     }
 }
