b315504930a2357d86ce10400523ddb0388c4973
jmseb3 <jmseb3@naver.com>
2025-10-26 19:17:17 +0900

    fix: 회원탈퇴 화면 키보드 UI 수정
    - `imePadding` 적용

--- app/src/main/java/com/example/planup/main/my/ui/MyPageOtherDeleteAccountView.kt
+++ app/src/main/java/com/example/planup/main/my/ui/MyPageOtherDeleteAccountView.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -58,7 +59,10 @@ fun MyPageOtherDeleteAccountView(
             style = Typography.Semibold_3XL
         )
 
-        Column {
+        Column(
+            modifier = Modifier
+                .imePadding()
+        ) {
             LazyColumn(
                 modifier = Modifier
                     .padding(vertical = 4.dp)
