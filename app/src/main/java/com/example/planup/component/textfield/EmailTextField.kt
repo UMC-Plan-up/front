31cbbcba576119bebb5e1939c759e32d1fdfa02c
jmseb3 <jmseb3@naver.com>
2025-12-07 18:54:37 +0900

    feat: 이메일 입력창 드롭다운 기능 추가
    - `EmailTextField` 컴포저블에 이메일 도메인을 선택할 수 있는 드롭다운 메뉴를 추가합니다.
    - 드롭다운 메뉴 아이템(`@gmail.com`, `@naver.com`, `@kakao.com` 등) 클릭 시, 입력된 텍스트에 따라 도메인을 추가하거나 교체하도록 구현합니다.
    - `@Preview`를 추가하여 `EmailTextField`의 시각적 테스트를 가능하게 합니다.

--- app/src/main/java/com/example/planup/component/textfield/EmailTextField.kt
+++ app/src/main/java/com/example/planup/component/textfield/EmailTextField.kt
@@ -2,24 +2,33 @@ package com.example.planup.component.textfield
 
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.input.TextFieldState
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringArrayResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextRange
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.planup.R
 import com.example.planup.theme.Black200
@@ -34,47 +43,91 @@ fun EmailTextField(
             state.text.isEmpty()
         }
     }
-
-    BasicTextField(
-        state = state,
-        modifier = Modifier
-            .fillMaxWidth()
-            .border(1.dp, Black200, RoundedCornerShape(4.dp))
-            .height(44.dp)
-            .padding(6.dp),
-        textStyle = Typography.Medium_SM,
-        decorator = { innerTextField ->
-            Box(modifier = Modifier.fillMaxWidth()) {
-                Box(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .align(Alignment.CenterStart)
-                        .padding(start = 8.dp, end = 8.dp),
-                    contentAlignment = Alignment.CenterStart
-                ) {
-                    if (showPlaceholderText) {
-                        Text(
-                            modifier = Modifier.align(Alignment.CenterStart),
-                            text = stringResource(R.string.email_enter),
-                            style = Typography.Medium_SM,
-                            color = Black200
-                        )
-                    }
-                    innerTextField()
-                    IconButton(
-                        modifier = Modifier.align(Alignment.CenterEnd),
-                        onClick = {
-
-                        }
+    var showDropDown by remember { mutableStateOf(false) }
+    Box() {
+        BasicTextField(
+            state = state,
+            modifier = Modifier
+                .fillMaxWidth()
+                .border(1.dp, Black200, RoundedCornerShape(4.dp))
+                .height(44.dp)
+                .padding(6.dp),
+            textStyle = Typography.Medium_SM,
+            decorator = { innerTextField ->
+                Box(modifier = Modifier.fillMaxWidth()) {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .align(Alignment.CenterStart)
+                            .padding(start = 8.dp, end = 8.dp),
+                        contentAlignment = Alignment.CenterStart
                     ) {
-                        Icon(
-                            painter = painterResource(R.drawable.arrow_down),
-                            contentDescription = null,
-                            tint = Color.Unspecified
-                        )
+                        if (showPlaceholderText) {
+                            Text(
+                                modifier = Modifier.align(Alignment.CenterStart),
+                                text = stringResource(R.string.email_enter),
+                                style = Typography.Medium_SM,
+                                color = Black200
+                            )
+                        }
+                        innerTextField()
+                        IconButton(
+                            modifier = Modifier.align(Alignment.CenterEnd),
+                            onClick = {
+                                showDropDown = true
+                            }
+                        ) {
+                            Icon(
+                                painter = painterResource(R.drawable.arrow_down),
+                                contentDescription = null,
+                                tint = Color.Unspecified
+                            )
+                        }
                     }
                 }
             }
+        )
+        DropdownMenu(
+            expanded = showDropDown,
+            onDismissRequest = {
+                showDropDown = false
+            }
+        ) {
+            listOf(
+                stringResource(R.string.dropdown_gmail),
+                stringResource(R.string.dropdown_naver),
+                stringResource(R.string.dropdown_kakao),
+            ).forEach { emailSuffix ->
+                DropdownMenuItem(
+                    text = {
+                        Text(emailSuffix)
+                    },
+                    onClick = {
+                        state.edit {
+                            val emailText = this.originalText.toString()
+                            if (!emailText.contains("@")) {
+                                this.append(emailSuffix)
+                            } else {
+                                val index = emailText.indexOf("@")
+                                this.replace(index, index + emailText.length, emailSuffix)
+                            }
+                        }
+                    }
+                )
+            }
         }
-    )
+    }
+}
+
+@Composable
+@Preview(showBackground = true, showSystemUi = true)
+private fun EmailTextFieldPreview() {
+    val state = remember { TextFieldState() }
+    Column(
+        modifier = Modifier.safeDrawingPadding()
+    ) {
+        EmailTextField(
+            state = state
+        )
+    }
 }
