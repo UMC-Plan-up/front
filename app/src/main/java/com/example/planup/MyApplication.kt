fd391258de42f96509b389c07604c02189eaffd4
Dohyeok Kim <hyeokdokim@gmail.com>
2026-01-17 11:49:21 +0900

    refactor: 중복된 Application 기능 이전

--- app/src/main/java/com/example/planup/MyApplication.kt
+++ app/src/main/java/com/example/planup/MyApplication.kt
@@ -14,39 +14,8 @@ class MyApplication : Application() {
 
         KakaoSdk.init(this, BuildConfig.KAKAO_NATIVE_KEY)
 
-        printKeyHash()
+
     }
 
-    private fun printKeyHash() {
-        try {
-            val packageInfo = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
-                packageManager.getPackageInfo(
-                    packageName,
-                    PackageManager.GET_SIGNING_CERTIFICATES
-                )
-            } else {
-                @Suppress("DEPRECATION")
-                packageManager.getPackageInfo(
-                    packageName,
-                    PackageManager.GET_SIGNATURES
-                )
-            }
 
-            val signatures = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
-                packageInfo.signingInfo?.apkContentsSigners
-            } else {
-                @Suppress("DEPRECATION")
-                packageInfo.signatures
-            }
-
-            signatures?.forEach { signature ->
-                val md = MessageDigest.getInstance("SHA")
-                md.update(signature.toByteArray())
-                val keyHash = Base64.encodeToString(md.digest(), Base64.NO_WRAP)
-                Log.d("KeyHash", "keyHash: $keyHash")
-            }
-        } catch (e: Exception) {
-            Log.e("KeyHash", "키 해시 얻기 실패", e)
-        }
-    }
 }
--- app/src/main/java/com/example/planup/network/App.kt
+++ app/src/main/java/com/example/planup/network/App.kt
@@ -2,12 +2,18 @@ package com.example.planup.network
 
 import android.app.Application
 import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Build
+import android.util.Base64
+import android.util.Log
 import coil3.ImageLoader
 import coil3.SingletonImageLoader
 import coil3.request.crossfade
 import com.example.planup.BuildConfig
 import com.example.planup.database.TokenSaver
+import com.kakao.sdk.common.KakaoSdk
 import dagger.hilt.android.HiltAndroidApp
+import java.security.MessageDigest
 import javax.inject.Inject
 
 @HiltAndroidApp
@@ -25,8 +31,8 @@ class App : Application() , SingletonImageLoader.Factory {
         super.onCreate()
         jwt = TokenManager(tokenSaver)
 
-        // TODO:: 상수화
-        com.kakao.sdk.common.KakaoSdk.init(this, BuildConfig.KAKAO_NATIVE_KEY)
+        KakaoSdk.init(this, BuildConfig.KAKAO_NATIVE_KEY)
+        printKeyHash()
     }
 
     override fun newImageLoader(context: Context): ImageLoader {
@@ -35,4 +41,36 @@ class App : Application() , SingletonImageLoader.Factory {
             .build()
     }
 
+    private fun printKeyHash() {
+        try {
+            val packageInfo = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                packageManager.getPackageInfo(
+                    packageName,
+                    PackageManager.GET_SIGNING_CERTIFICATES
+                )
+            } else {
+                @Suppress("DEPRECATION")
+                packageManager.getPackageInfo(
+                    packageName,
+                    PackageManager.GET_SIGNATURES
+                )
+            }
+
+            val signatures = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                packageInfo.signingInfo?.apkContentsSigners
+            } else {
+                @Suppress("DEPRECATION")
+                packageInfo.signatures
+            }
+
+            signatures?.forEach { signature ->
+                val md = MessageDigest.getInstance("SHA")
+                md.update(signature.toByteArray())
+                val keyHash = Base64.encodeToString(md.digest(), Base64.NO_WRAP)
+                Log.d("KeyHash", "keyHash: $keyHash")
+            }
+        } catch (e: Exception) {
+            Log.e("KeyHash", "키 해시 얻기 실패", e)
+        }
+    }
 }
