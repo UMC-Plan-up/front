69a537ea13ad2af7d6e1306a2ce7cac90784d01c
Dohyeok Kim <hyeokdokim@gmail.com>
2026-01-18 16:49:27 +0900

    feat: 초대코드 api 연결

--- app/src/main/java/com/example/planup/onboarding/InviteCodeViewModel.kt
+++ app/src/main/java/com/example/planup/onboarding/InviteCodeViewModel.kt
@@ -1,19 +1,34 @@
 package com.example.planup.onboarding
 
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.planup.database.TokenSaver
 import com.example.planup.main.user.domain.UserRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class InviteCodeViewModel @Inject constructor(
     private val userRepository: UserRepository
 ) : ViewModel() {
-    
+
     private val _inviteCode: MutableStateFlow<String> = MutableStateFlow("")
     val inviteCode: StateFlow<String> = _inviteCode
+
+    init {
+        viewModelScope.launch {
+            val code = userRepository.getInviteCode()
+            println("inviteCode = $code")
+            _inviteCode.update { code }
+        }
+    }
 
 
 }
--- app/src/main/java/com/example/planup/onboarding/OnBoardingShareCodeScreen.kt
+++ app/src/main/java/com/example/planup/onboarding/OnBoardingShareCodeScreen.kt
@@ -3,10 +3,8 @@ package com.example.planup.onboarding
 import android.content.ClipData
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
@@ -14,8 +12,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.text.input.rememberTextFieldState
@@ -28,7 +24,6 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ClipEntry
