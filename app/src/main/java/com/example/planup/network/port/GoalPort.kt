60a6d8400b8df3a3e92052e825459ec0219b047b
jaeminKim <kg8480@gmail.com>
2026-01-18 20:37:03 +0900

    deprecated 코드 정리

--- app/src/main/java/com/example/planup/network/port/GoalPort.kt
+++ app/src/main/java/com/example/planup/network/port/GoalPort.kt
@@ -1,9 +1,5 @@
 package com.example.planup.network.port
 
-import com.example.planup.main.goal.data.ApiResponseListMyGoalListDto
-import retrofit2.Call
-import retrofit2.http.GET
-
 @Deprecated(
     message = "Use GoalApi instead",
     replaceWith = ReplaceWith("GoalApi")
