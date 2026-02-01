914b537b926796a34874d2251863bc2205e84d2d
jaeminKim <kg8480@gmail.com>
2026-02-01 16:55:39 +0900

    요청 값 수정

--- app/src/main/java/com/example/planup/network/GoalApi.kt
+++ app/src/main/java/com/example/planup/network/GoalApi.kt
@@ -41,13 +41,13 @@ interface GoalApi {
     // 목표 삭제 API
     @DELETE("/goals/{goalId}")
     suspend fun deleteGoal(
-        @Path("goalId") goalId: Int
+        @Query("goalId") goalId: Int
     ): Response<GoalEditResponse>
 
     // 목표 공개 & 비공개 API
     @PATCH("/goals/{goalId}/public")
     suspend fun setGoalPublic(
-        @Path("goalId") goalId: Int
+        @Query("goalId") goalId: Int
     ): Response<GoalEditResponse>
 
     // 목표 활성 & 비활성화 API
@@ -71,16 +71,18 @@ interface GoalApi {
         @Query("goalCategory") goalCategory: String
     ): Response<GoalListResponseDto>
 
+    // http://54.180.207.84/community/daily-achievement?targetDate=2024-08-21
     @GET("/goals/daily/{date}")
     suspend fun getDailyGoal(
-        @Path("date") date: String
+        @Query("date") date: String
     ): Response<DailyGoalResponse>
-
+    //http://54.180.207.84/community/daily-achievement?targetDate=2024-08-21
     @GET("/community/daily-achievement")
     suspend fun getDailyAchievement(
         @Query("targetDate") targetDate: String
     ): Response<DailyAchievementResponse>
 
+    //http://54.180.207.84/community/1/total-achievement
     @GET("/community/{goalId}/total-achievement")
     suspend fun getTotalAchievement(
         @Path("goalId") goalId: Int
@@ -96,9 +98,10 @@ interface GoalApi {
     suspend fun getFriendPhotos(
         @Path("friendId") friendId: Int,
         @Path("goalId") goalId: Int,
-        @Query("userId") userId: Int
+        @Query("userId") userId: Int // ? 스웨거에 안보임
     ): FriendPhotosResponse
 
+    // http://54.180.207.84/goals/1/comments
     @POST("/goals/{goalId}/comment")
     suspend fun createComment(
         @Path("goalId") goalId: Int,
@@ -111,9 +114,10 @@ interface GoalApi {
         @Body editGoalRequest: EditGoalRequest
     ): EditGoalApiResponse
 
+    //http://54.180.207.84/goals/{goalId}?goalId=1
     @GET("/goals/{goalId}/edit")
     suspend fun getEditGoal(
-        @Path("goalId") goalId: Int
+        @Query("goalId") goalId: Int
     ): Response<EditGoalApiResponse>
 
     // 내 목표 상세 조회 API
@@ -122,39 +126,42 @@ interface GoalApi {
         @Path("goalId") goalId: Int
     ): ApiResponseListMyGoalListDto
 
+    // http://54.180.207.84/goals/mygoal/{goalId}?goalId=1
     @GET("/goals/mygoal/{goalId}")
     suspend fun getGoalDetail(
-        @Path("goalId") goalId: Int
+        @Query("goalId") goalId: Int
     ): GoalDetailResponse
 
+    // http://54.180.207.84/goals/1/friendstimer
     @GET("/goals/{goalId}/friendstimer")
     suspend fun getFriendsTimer(
         @Path("goalId") goalId: Int
     ): Response<FriendsTimerResponse>
 
+    // http://54.180.207.84/goals/1/memo/1
     @GET("/goals/{goalId}/memo/{date}")
     suspend fun getDateMemo(
         @Path("goalId") goalId: Int,
         @Path("date") date: String
     ): Response<DateMemoResponse>
-
+    // http://54.180.207.84/goals/1/memo
     @POST("goals/{goalId}/memo")
     suspend fun saveMemo(
         @Path("goalId") goalId: Int,
         @Body request: MemoRequest
     ): Response<PostMemoResponse>
-
+    // http://54.180.207.84/goals/1/comments
     @GET("/goals/{goalId}/comments")
     suspend fun getComments(
         @Path("goalId") goalId: Int
     ): GetCommentsResponse
-
+    //http://54.180.207.84/community/friend/2/goal/1/total-achievement
     @GET("/community/friend/{friendId}/goal/{goalId}/total-achievement")
     suspend fun getFriendGoalAchievement(
         @Path("friendId") friendId: Int,
         @Path("goalId") goalId: Int
     ): Response<FriendGoalAchievementResponse>
-
+    //http://54.180.207.84/goals/{goalId}/photos?goalId=1
     @GET("/goals/{goalId}/photos")
     suspend fun getGoalPhotos(
         @Path("goalId") goalId: Int
