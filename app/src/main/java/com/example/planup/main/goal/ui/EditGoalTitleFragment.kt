53435c4fc159d13242e7f6984ebde51a784f0f1a
jaeminKim <kg8480@gmail.com>
2026-02-01 16:54:14 +0900

    안쓰는 속성 삭제

--- app/src/main/java/com/example/planup/main/goal/ui/EditGoalTitleFragment.kt
+++ app/src/main/java/com/example/planup/main/goal/ui/EditGoalTitleFragment.kt
@@ -1,6 +1,5 @@
 package com.example.planup.main.goal.ui
 
-import android.content.SharedPreferences
 import android.os.Bundle
 import android.text.Editable
 import android.util.Log
@@ -12,17 +11,16 @@ import androidx.lifecycle.lifecycleScope
 import com.example.planup.R
 import com.example.planup.databinding.FragmentEditGoalTitleBinding
 import com.example.planup.main.goal.item.EditGoalResponse
-import com.example.planup.main.goal.item.GoalRetrofitInstance
-import com.example.planup.main.goal.item.GoalApiService
 import kotlinx.coroutines.launch
 import android.text.TextWatcher
+import com.example.planup.network.RetrofitInstance
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class EditGoalTitleFragment : Fragment() {
     private lateinit var binding: FragmentEditGoalTitleBinding
 
     private var goalId: Int = 0
-    private lateinit var prefs: SharedPreferences
-
     private var isSolo: Boolean = false
     private var goalData: EditGoalResponse? = null
 
@@ -30,6 +28,7 @@ class EditGoalTitleFragment : Fragment() {
         super.onCreate(savedInstanceState)
         // arguments에서 goalId 받아오기
         goalId = arguments?.getInt("goalId") ?: 0
+
         Log.d("EditGoalTitleFragment", "goalId: $goalId")
 
         isSolo = arguments?.getBoolean("isSolo") ?: false
@@ -40,16 +39,13 @@ class EditGoalTitleFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentEditGoalTitleBinding.inflate(inflater, container, false)
-        val view = inflater.inflate(R.layout.fragment_edit_goal_title, container, false)
-        prefs = requireActivity().getSharedPreferences("userInfo", android.content.Context.MODE_PRIVATE)
-        val token = prefs.getString("accessToken", null)
-        getGoalEditData(token, goalId)
+
+        getGoalEditData(goalId)
 
         val titleEt = binding.editFriendGoalNameEt
         val goalAmountEt = binding.editFriendGoalPeriodEt
         val nextBtn = binding.editFriendGoalNextBtn
         val backBtn = binding.editFriendBackArrowIv
-
         backBtn.setOnClickListener {
             if(isSolo) parentFragmentManager.popBackStack()
             else requireActivity().finish()
@@ -140,7 +136,6 @@ class EditGoalTitleFragment : Fragment() {
 
             val nextFragment = EditGoalTimerFragment()
             nextFragment.arguments = bundle
-
             parentFragmentManager.beginTransaction()
                 .replace(R.id.edit_friend_goal_fragment_container, nextFragment)
                 .addToBackStack(null)
@@ -179,18 +174,17 @@ class EditGoalTitleFragment : Fragment() {
         return binding.root
     }
 
-    private fun getGoalEditData(token: String?, goalId: Int) {
+    private fun getGoalEditData(goalId: Int) {
         lifecycleScope.launch {
             try {
-                val apiService = GoalRetrofitInstance.api.create(GoalApiService::class.java)
-                val response = apiService.getEditGoal(token = "Bearer $token", goalId = goalId)
-                if (response.isSuccess) {
-                    goalData = response.result
+                val response = RetrofitInstance.goalApi.getEditGoal(goalId = goalId)
+                if (response.isSuccessful) {
+                    goalData = response.body()?.result
                     // 기존 값으로 EditText 초기화
                     binding.editFriendGoalNameEt.setText(goalData?.goalName)
                     binding.editFriendGoalPeriodEt.setText(goalData?.oneDose.toString())
                 } else {
-                    Log.d("EditGoalTitleFragment", "API 실패: ${response.message}")
+                    Log.d("EditGoalTitleFragment", "API 실패: ${response.message()}")
                 }
             } catch (e: Exception) {
                 e.printStackTrace()
