6d4502c0b62dff0bb8f7a6540e3010afacf9a07d
jaeminKim <kg8480@gmail.com>
2026-02-01 17:01:05 +0900

    필요 함수 삽입

--- app/src/main/java/com/example/planup/main/goal/domain/GoalRepository.kt
+++ app/src/main/java/com/example/planup/main/goal/domain/GoalRepository.kt
@@ -3,7 +3,9 @@ package com.example.planup.main.goal.domain
 import com.example.planup.goal.data.GoalCreateRequest
 import com.example.planup.goal.data.GoalCreateResponse
 import com.example.planup.goal.data.GoalResult
+import com.example.planup.main.goal.item.EditGoalApiResponse
 import com.example.planup.main.goal.item.EditGoalRequest
+import com.example.planup.main.goal.item.EditGoalResponse
 import com.example.planup.main.goal.item.MyGoalListItem
 import com.example.planup.network.ApiResult
 import retrofit2.Response
@@ -17,4 +19,5 @@ interface GoalRepository {
     suspend fun createGoal(
         goalCreateRequest: GoalCreateRequest
     ): ApiResult<GoalResult>
+    suspend fun getGoalDetail(goalId: Int): ApiResult<EditGoalResponse>
 }
