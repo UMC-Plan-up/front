a2f87fbfb955dd145c9187d350f2923c84d60a17
jaeminKim <kg8480@gmail.com>
2026-02-08 17:35:47 +0900

    통합용 프래그먼트

---
+++ app/src/main/java/com/example/planup/goal/ui/TopTitleFragment.kt
@@ -0,0 +1,40 @@
+package com.example.planup.goal.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.planup.R
+import com.example.planup.databinding.FragmentTopTitleBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class TopTitleFragment : Fragment() {
+
+    lateinit var binding: FragmentTopTitleBinding
+    var title: String? = null
+    var mode: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        title = arguments?.getString("user") ?: "사용자"
+        mode = arguments?.getString("mode")
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentTopTitleBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // 뒤로가기
+        binding.backIcon.setOnClickListener { parentFragmentManager.popBackStack() }
+    }
+
+}
