c1994fcded88d9da01244d5c8cda0d6339a31aad
jaeminKim <kg8480@gmail.com>
2026-02-27 20:58:07 +0900

    [feat] 댓글 리스트 구조 업로드 어댑터

---
+++ app/src/main/java/com/planup/planup/goal/adapter/CommentAdapter.kt
@@ -0,0 +1,37 @@
+package com.planup.planup.goal.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.planup.planup.R
+import com.planup.planup.goal.util.loadProfile
+
+data class CommentItem(val profile: String?,val nickname: String, val comment: String)
+
+class CommentAdapter(private val comments: List<CommentItem>)
+    : RecyclerView.Adapter<CommentViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CommentViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_comment, parent, false)
+        return CommentViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: CommentViewHolder, position: Int) {
+        comments[position].run {
+            holder.profileImage.loadProfile(profile)
+            holder.nicknameText.text = nickname
+            holder.commentText.text = comment
+        }
+    }
+
+    override fun getItemCount(): Int = comments.size
+}
+
+class CommentViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    val profileImage: ImageView = itemView.findViewById(R.id.comment_profile)
+    val nicknameText: TextView = itemView.findViewById(R.id.comment_nickname)
+    val commentText: TextView = itemView.findViewById(R.id.textSpeech)
+}
