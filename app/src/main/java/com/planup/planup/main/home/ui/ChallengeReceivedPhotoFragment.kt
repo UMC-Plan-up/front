dd13a497e7095ebaf2a5a2bbb0198762e49e20e6
jaeminKim <kg8480@gmail.com>
2026-03-01 07:19:56 +0900

    [feat] 통합 xml로 변경

--- app/src/main/java/com/planup/planup/main/home/ui/ChallengeReceivedPhotoFragment.kt
+++ app/src/main/java/com/planup/planup/main/home/ui/ChallengeReceivedPhotoFragment.kt
@@ -18,6 +18,7 @@ import com.planup.planup.goal.util.challengeReceivedInit
 import com.planup.planup.goal.util.errorToast
 import com.planup.planup.goal.util.initClickListener
 import com.planup.planup.goal.util.makeToast
+import com.planup.planup.goal.util.challengeReceivedInit
 import com.planup.planup.main.home.data.ChallengeReceivedPhoto
 import com.planup.planup.main.MainActivity
 
